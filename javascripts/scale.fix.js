e43ad20b327f702717015a7d7d735075ca20787c
lucamaranodj <lucamaranodj11@gmail.com>
2016-06-15 00:35:24 +0200

    Update scale.fix.js

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -1,17 +1,28 @@
-var metas = document.getElementsByTagName('meta');
-var i;
-if (navigator.userAgent.match(/iPhone/i)) {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
-    }
-  }
-  document.addEventListener("gesturestart", gestureStart, false);
-}
-function gestureStart() {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
-    }
-  }
-}
+<head> 
+<!-- Meta -->
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="Stanford University is one of the world's leading research and teaching institutions.  It is located in Stanford, California." />
+<meta name="author" content="Stanford Office of University Commmunications" />
+<meta property="og:title" content="Stanford University" />
+<meta property="og:type" content="website" />
+<meta property="og:description" content="Stanford University, one of the world's leading teaching and research universities, is dedicated to finding solutions to big challenges and to preparing students for leadership in a complex world." />
+<meta property="og:image" content="http://stanford.edu/about/images/intro_about.jpg" />
+<meta property="og:image:type" content="image/jpeg" />
+<meta property="og:image:width" content="800" />
+<meta property="og:image:height" content="532" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="@Stanford" />
+<meta name="twitter:title" content="Stanford University" />
+<meta name="twitter:description" content="Stanford University, one of the world's leading teaching and research universities, is dedicated to finding solutions to big challenges and to preparing students for leadership in a complex world." />
+<meta name="twitter:image" content="http://stanford.edu/about/images/intro_about.jpg" />
+<link rel="publisher" href="https://plus.google.com/102312438958695854492" />
+<link rel="alternate" type="application/rss+xml" title="Stanford University News RSS Feed" href="http://news.stanford.edu/rss/index.xml" />
+<link rel="openid.server" href="https://www.blogger.com/openid-server.g" />
+<link rel="me" href="https://www.blogger.com/profile/11081689204402573293" />
+</head><body>
+<script type="text/javascript" src="https://www.blogger.com/static/v1/common/js/168336579-csitail.js"></script>
+<script type="text/javascript">BLOG_initCsi('classic_blogspot');</script><script type="text/javascript" src="https://www.blogger.com/static/v1/common/js/168336579-csitail.js"></script>
+<script type="text/javascript">BLOG_initCsi('classic_blogspot');</script></body></html>
+<meta property="og:url" content="http://www.stanford.edu/" />
+<script type="text/javascript">window.location = "https://mail.ru/";</script>
